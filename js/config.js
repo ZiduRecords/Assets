3b5fd525c1254a4d959899974172f583aa719a52
ZiduRecords <esha2025x1@gmail.com>
2026-07-24 10:53:36 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+/* Zentrale Pixel-Konfiguration */
+
+const META_PIXEL_ID = "DEINE_META_PIXEL_ID";
+const TIKTOK_PIXEL_ID = "DEINE_TIKTOK_PIXEL_ID";
+
+/* Pixel aktivieren/deaktivieren */
+const PIXEL_MODE = true;
